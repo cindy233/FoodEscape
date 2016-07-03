fdb6bc8da2a36c4c5e460b28c6d3c43f882e160e
Xinlei <xttzhao@gmail.com>
2016-07-03 20:44:18 +0800

    Potato is jumping and moving with player in battle scene.

--- Assets/FollowingPlayer.cs
+++ Assets/FollowingPlayer.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class FollowingPlayer : MonoBehaviour {
 
 	public GameObject player;
-
+	public float maxSpeed;
 	private Vector3 offset;
+	private Vector3 startPosition;
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position - player.transform.position;
+		startPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = player.transform.position + offset;
+		transform.position = new Vector3(transform.position.x, startPosition.y + Mathf.Sin(Time.time * maxSpeed), transform.position.z);
+
 	}
+
 }

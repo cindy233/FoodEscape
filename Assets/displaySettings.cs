b6d1442fb1354943519bcc4aea0e95e676cd2115
blueMasuda <elena@Mac.local>
2016-07-05 00:48:05 +0800

    Choose character scene is added. Added two characters, collision detection, mouse click.
    - Character selection scene is added
    - Click on characters to select
    - Currently 2 characters are added

---
+++ Assets/displaySettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displaySettings : MonoBehaviour {
+
+	private int selected;
+	// Use this for initialization
+	void Start () {
+		selected = PlayerPrefs.GetInt ("selectedCharacter");
+		if (selected == 2)
+			gameObject.SetActive (true);
+		else
+			gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

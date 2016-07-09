95bef52e0caffbaf19532b63a6a0345f654dec67
Xinlei <xttzhao@gmail.com>
2016-07-09 15:15:07 +0800

    Butcher can play attack animation when distance between butcher and taichi is less than 20.

---
+++ Assets/Scripts/ButcherAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButcherAttack : MonoBehaviour {
+	public GameObject player;
+	//need a condition statement to apply to different characters.
+	private float dist;
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(player.transform.position, transform.position);
+		print("Distance to other: " + dist);
+		if (dist <= 15) {
+			print ("Attack");
+			anim.Play("Attack", -1, 0f);
+		}
+	}
+		
+}
+

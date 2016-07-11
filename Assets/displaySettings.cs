842de68b2ae87e64247df1b13e7436b9cc587ecc
blueMasuda <elena@Blue.local>
2016-07-11 21:59:59 +0800

    Batcher will attack different characters

--- Assets/displaySettings.cs
+++ Assets/displaySettings.cs
@@ -6,13 +6,16 @@ public class displaySettings : MonoBehaviour {
 	private int selected;
 	private GameObject player;
 	private GameObject Capsule;
+	private GameObject Butcher;
 
 	// Use this for initialization
 	void Start () {
 		
 		selected = PlayerPrefs.GetInt ("selectedCharacter");
 		Capsule = GameObject.Find ("Potato");
+		Butcher = GameObject.Find ("TheButcher");
 		FollowingPlayer fplayer = Capsule.GetComponent<FollowingPlayer> ();
+		ButcherAttack butcherAttack = Butcher.GetComponent<ButcherAttack> ();
 
 		switch (selected) {
 		case 1:
@@ -20,6 +23,7 @@ public class displaySettings : MonoBehaviour {
 			player.SetActive (true);
 			transform.FindChild ("unitychan").gameObject.SetActive (false);
 			transform.FindChild ("querychan").gameObject.SetActive (false);
+			butcherAttack.player = player;
 			break;
 		case 2:
 			player = transform.FindChild ("unitychan").gameObject;
@@ -27,6 +31,7 @@ public class displaySettings : MonoBehaviour {
 			transform.FindChild ("taichi1").gameObject.SetActive (false);
 			transform.FindChild ("unitychan").gameObject.SetActive (true);
 			transform.FindChild ("querychan").gameObject.SetActive (false);
+			butcherAttack.player = player;
 			break;
 		case 3:
 			player = transform.FindChild ("querychan").gameObject;
@@ -34,6 +39,7 @@ public class displaySettings : MonoBehaviour {
 			transform.FindChild ("taichi1").gameObject.SetActive (false);
 			transform.FindChild ("unitychan").gameObject.SetActive (false);
 			transform.FindChild ("querychan").gameObject.SetActive (true);
+			butcherAttack.player = player;
 			break;
 		default:
 			break;

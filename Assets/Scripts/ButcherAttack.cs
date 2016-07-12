4ddc6df6e7f6bc60d08bd38246906c16e3f97a11
V Zhao <V@Vs-MacBook-Pro.local>
2016-07-12 12:20:23 +0800

    Butcher can walk (but butcher position needs to be updated)

--- Assets/Scripts/ButcherAttack.cs
+++ Assets/Scripts/ButcherAttack.cs
@@ -6,6 +6,8 @@ public class ButcherAttack : MonoBehaviour {
 	//need a condition statement to apply to different characters.
 	private float dist;
 	public Animator anim;
+	private float nextActionTime = 0.0f;
+	public float period = 0.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,10 +17,22 @@ public class ButcherAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float dist = Vector3.Distance(player.transform.position, transform.position);
+		if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+			if (dist >= 20) {
+				anim.Play ("Attack", -1, 0f);
+
+		}
 		//print("Distance to other: " + dist);
-		if (dist >= 20) {
+		/*if (dist >= 20) {
 			anim.Play ("Attack", -1, 0f);
 		}
+*/
+		/*if (Input.GetKeyDown("5")) {
+			print ("5 is pressed");
+			anim.Play ("HumanoidWalk",0,0f);
+		}
+		*/
 
 	}
 		

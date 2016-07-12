558c1f3d1923199b6e4df77effafb7094c158e97
V Zhao <V@Vs-MacBook-Pro.local>
2016-07-12 12:29:17 +0800

    Trying check distance every 2 seconds for butcher.

--- Assets/Scripts/ButcherAttack.cs
+++ Assets/Scripts/ButcherAttack.cs
@@ -6,8 +6,8 @@ public class ButcherAttack : MonoBehaviour {
 	//need a condition statement to apply to different characters.
 	private float dist;
 	public Animator anim;
-	private float nextActionTime = 0.0f;
-	public float period = 0.1f;
+	private float nextActionTime = 0.1f;
+	public float period = 20f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,25 +16,20 @@ public class ButcherAttack : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float dist = Vector3.Distance(player.transform.position, transform.position);
-		if (Time.time > nextActionTime ) {
-			nextActionTime += period;
-			if (dist >= 20) {
+		float dist = Vector3.Distance (player.transform.position, transform.position);
+		if (Time.time > nextActionTime) {
+			nextActionTime = Time.time + period;
+			if (dist <= 20) {
 				anim.Play ("Attack", -1, 0f);
 
-		}
-		//print("Distance to other: " + dist);
-		/*if (dist >= 20) {
-			anim.Play ("Attack", -1, 0f);
-		}
-*/
-		/*if (Input.GetKeyDown("5")) {
+			}
+			//print("Distance to other: " + dist);
+
+			/*if (Input.GetKeyDown("5")) {
 			print ("5 is pressed");
 			anim.Play ("HumanoidWalk",0,0f);
 		}
 		*/
-
+		}
 	}
-		
 }
-

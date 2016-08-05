1e06a46905b9c2a9c401dd53f3bb46cda1f2cceb
blueMasuda <gameprince.tt@gmail.com>
2016-08-05 10:18:46 +0800

    Add story background scene and change scene linkage
    Add story background scene and change scene linkage

---
+++ Assets/Scripts/displayScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class displayScript : MonoBehaviour {
+
+	public Text scriptDisplay;
+	public TextAsset wholeScript;
+	public GameObject btn;
+	private int word = 0;
+	private string[] letters;
+	private bool addwords = false;
+
+	// Use this for initialization
+	void Start () {
+
+		letters = wholeScript.text.Split (' ');
+		scriptDisplay.text = letters [0];
+		word = 1;
+		StartCoroutine (slower ());
+		btn.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (word < letters.Length && addwords) {
+		
+			scriptDisplay.text += " " + letters [word++];
+			StartCoroutine (slower ());
+		
+		} else if (word >= letters.Length)
+			btn.SetActive (true);
+	}
+	IEnumerator slower(){
+	
+		addwords = false;
+		yield return new WaitForSeconds(0.1f);
+		addwords = true;
+	}
+}
---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+
+	public string sceneName = "Start";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+
+			onClick ();
+
+		}
+	}
+
+	public void onClick(){
+	
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
